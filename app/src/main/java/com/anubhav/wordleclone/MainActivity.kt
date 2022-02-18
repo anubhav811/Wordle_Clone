0c00f40ebe26750e13c200ce85e57871ac7b5eae
Anubhav Chachra <chachraanubhav@gmail.com>
2022-02-18 10:33:47 +0530

    focus change on et working , backspace yet todo

--- app/src/main/java/com/anubhav/wordleclone/MainActivity.kt
+++ app/src/main/java/com/anubhav/wordleclone/MainActivity.kt
@@ -1,21 +1,63 @@
 package com.anubhav.wordleclone
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.AttributeSet
 import android.util.SparseArray
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputConnection
-import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.addTextChangedListener
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.keyboard.*
+import kotlinx.android.synthetic.main.keyboard.view.*
+
 
 class MainActivity : AppCompatActivity() {
 
-    val WORD = "tiger"
+    val WORD = "TIGER"
+    val keys: SparseArray<Any> = SparseArray()
+    var currRow = 1;
+
+    private var inputConnection: InputConnection? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        if (currRow == 1) {
+            val ic: InputConnection = et_11.onCreateInputConnection(EditorInfo())
+            keyboard.setInputConnection(ic)
+            setEtChanger(et_11, et_12)
+            setEtChanger(et_12, et_13)
+            setEtChanger(et_13, et_14)
+            setEtChanger(et_14, et_15)
+            setEtChanger(et_15, et_21)
+        }
     }
 
 
+    private fun setEtChanger(et1: EditText, et2: EditText) {
+
+        et1.addTextChangedListener(object : TextWatcher {
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (et1.text.toString().length == 1) {
+                    et2.requestFocus()
+                    val ic: InputConnection = et2.onCreateInputConnection(EditorInfo())
+                    keyboard.setInputConnection(ic)
+                }
+            }
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun afterTextChanged(s: Editable?) {}
+        })
+    }
 
 
-}
+    }
+
+
---
+++ app/src/main/java/com/anubhav/wordleclone/MyKeyboard.kt
@@ -0,0 +1,112 @@
+package com.anubhav.wordleclone
+
+import android.content.Context
+import android.text.TextUtils
+import android.util.AttributeSet
+import android.util.SparseArray
+import android.view.LayoutInflater
+import android.view.View
+import android.view.inputmethod.InputConnection
+import android.widget.LinearLayout
+import kotlinx.android.synthetic.main.keyboard.view.*
+
+class MyKeyboard @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) :
+    LinearLayout(context, attrs, defStyleAttr), View.OnClickListener {
+    val keys : SparseArray<Any> = SparseArray()
+    private var inputConnection: InputConnection? = null
+
+    private fun init(context: Context, attrs: AttributeSet?) {
+     LayoutInflater.from(context).inflate(R.layout.keyboard, this, true);
+
+        button_q.setOnClickListener(this)
+        button_w.setOnClickListener(this)
+        button_e.setOnClickListener(this)
+        button_r.setOnClickListener(this)
+        button_t.setOnClickListener(this)
+        button_y.setOnClickListener(this)
+        button_u.setOnClickListener(this)
+        button_i.setOnClickListener(this)
+        button_o.setOnClickListener(this)
+        button_p.setOnClickListener(this)
+        button_a.setOnClickListener(this)
+        button_s.setOnClickListener(this)
+        button_d.setOnClickListener(this)
+        button_f.setOnClickListener(this)
+        button_g.setOnClickListener(this)
+        button_h.setOnClickListener(this)
+        button_j.setOnClickListener(this)
+        button_k.setOnClickListener(this)
+        button_l.setOnClickListener(this)
+        button_z.setOnClickListener(this)
+        button_x.setOnClickListener(this)
+        button_c.setOnClickListener(this)
+        button_v.setOnClickListener(this)
+        button_b.setOnClickListener(this)
+        button_n.setOnClickListener(this)
+        button_m.setOnClickListener(this)
+
+        button_enter.setOnClickListener(this)
+        button_delete.setOnClickListener(this)
+        
+        keys.put(R.id.button_q,"Q")
+        keys.put(R.id.button_w,"W")
+        keys.put(R.id.button_e,"E")
+        keys.put(R.id.button_r,"R")
+        keys.put(R.id.button_t,"T")
+        keys.put(R.id.button_y,"Y")
+        keys.put(R.id.button_u,"U")
+        keys.put(R.id.button_i,"I")
+        keys.put(R.id.button_o,"O")
+        keys.put(R.id.button_p,"P")
+        keys.put(R.id.button_a,"A")
+        keys.put(R.id.button_s,"S")
+        keys.put(R.id.button_d,"D")
+        keys.put(R.id.button_f,"F")
+        keys.put(R.id.button_g,"G")
+        keys.put(R.id.button_h,"H")
+        keys.put(R.id.button_j,"J")
+        keys.put(R.id.button_k,"K")
+        keys.put(R.id.button_l,"L")
+        keys.put(R.id.button_z,"Z")
+        keys.put(R.id.button_x,"X")
+        keys.put(R.id.button_c,"C")
+        keys.put(R.id.button_v,"V")
+        keys.put(R.id.button_b,"B")
+        keys.put(R.id.button_n,"N")
+        keys.put(R.id.button_m,"M")
+    }
+
+
+override fun onClick(view: View) {
+    if (inputConnection == null) return
+    if (view.id == R.id.button_delete) {
+        val selectedText = inputConnection!!.getSelectedText(0)
+        if (TextUtils.isEmpty(selectedText)) {
+            inputConnection!!.deleteSurroundingText(1, 0)
+        } else {
+            inputConnection!!.commitText("", 1)
+        }
+    } else {
+        val value = keys[view.id]
+        inputConnection!!.commitText(value as CharSequence?, 1)
+    }
+}
+
+fun setInputConnection(ic: InputConnection?) {
+    inputConnection = ic
+}
+
+init {
+    init(context, attrs)
+}
+}
+
+
+
+
+
+
